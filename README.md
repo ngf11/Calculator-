2c251c5f47d2cbe8b17dc7c192d9221a22c09044
Nicolas Fuentes <nfuentes11@gmail.com>
2023-03-14 00:33:56 -0400

    Calculator fully functional. Need to add keyboard functionality

---
+++ README.md
@@ -0,0 +1,1 @@
+decimals- multiplications, subtractions,dev
--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 function operate(operator, a, b) {
   if (operator === "+") {
-    return sum(a, b);
+    return add(a, b);
   } else if (operator === "-") {
     return subtract(a, b);
-  } else if (operator === "*") {
+  } else if (operator === "x") {
     return multiply(a, b);
-  } else if (operator === "/") {
-    return divide(a, b);
+  } else if (operator === "÷") {
+    if (b === 0) {
+      console.log("Division by zero!");
+      document.getElementById("zero").style.display = "block";
+      return "Error";
+    } else {
+      return divide(a, b);
+    }
   } else {
     return "Error";
   }
 }
 
-function sum(a, b) {
+function add(a, b) {
   return a + b;
 }
 function subtract(a, b) {
@@ -26,6 +32,10 @@ function divide(a, b) {
   return a / b;
 }
 
+function closeModal() {
+  zeroModal.style.display = "none";
+}
+
 const displayPrevious = document.querySelector(".display1");
 const displayCurrent = document.querySelector(".display2");
 const operators = document.querySelectorAll(".operators");
@@ -33,6 +43,10 @@ const operands = document.querySelectorAll(".operands");
 const equals = document.getElementById("equals");
 const clearButton = document.getElementById("clear-all");
 const deleteButton = document.getElementById("deleteLastInput");
+const zeroModal = document.getElementById("zero");
+const zeroCloseButton = zeroModal.querySelector("button");
+zeroCloseButton.addEventListener("click", closeModal);
+let operator;
 let firstValue;
 let secondValue;
 
@@ -41,18 +55,32 @@ let secondValue;
 for (let i = 0; i < operands.length; i++) {
   operands[i].addEventListener("click", function (e) {
     displayCurrent.innerText += this.innerText;
-    e.target.innerText;
+
+    if (!operator) {
+      firstValue = parseFloat(displayCurrent.innerText);
+      console.log("firstValue:", firstValue);
+    } else {
+      secondValue = parseFloat(displayCurrent.innerText);
+      console.log("secondValue:", secondValue);
+    }
   });
 }
 
 for (let i = 0; i < operators.length; i++) {
   operators[i].addEventListener("click", function () {
     displayCurrent.innerText += this.innerText;
+    operator = this.innerText;
+    firstValue = parseFloat(displayCurrent.innerText);
+    displayCurrent.innerText = "";
+    secondValue = null;
   });
 }
 clearButton.addEventListener("click", (e) => {
   displayCurrent.innerText = "";
   displayPrevious.innerText = "";
+  operator = null;
+  firstValue = null;
+  secondValue = null;
 });
 
 deleteButton.addEventListener("click", (e) => {
@@ -61,5 +89,11 @@ deleteButton.addEventListener("click", (e) => {
 
 equals.addEventListener("click", (e) => {
   displayPrevious.innerText = displayCurrent.innerText;
-  displayCurrent.innerText = operate();
+  let result = operate(operator, firstValue, secondValue);
+  displayCurrent.innerText = result.toFixed(2);
+  displayPrevious.innerText = `${firstValue} ${operator} ${secondValue}`;
+  firstValue = parseFloat(displayCurrent.innerText);
+  secondValue = null;
+  operator = null;
 });
+// firstValue == 0 && operator == "÷" && secondValue == 0
